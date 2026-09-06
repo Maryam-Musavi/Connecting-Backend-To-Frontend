e50978dde0e3732bda2151cec3b0be215d290000
M.Musavi <maryam.a.musavi@gmail.com>
2026-09-06 19:51:09 +0330

    Practicing PHP

--- backend/register.php
+++ backend/register.php
@@ -3,25 +3,22 @@ header('Content-Type: application/json; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
-    $raw = file_get_contents((string)($data['name'] ?? ''));
-    $data = json_decode($raw, true);
-
-    if (!is_array($data)) {
-        $data = $_POST;
-    }
-
-    $name = trim((string)($data['name']??''));
-    $post = trim((string)($data['phone']??''));
-
-
-
-
-
     echo json_encode(['success' => false, 'message' => 'فقط POST مجاز است'], JSON_UNESCAPED_UNICODE);
     exit;
 }
 
-echo json_encode(['success' => true, 'message' =>' بک اند کار میکند!'], JSON_UNESCAPED_UNICODE);
+$raw = file_get_contents('php://input');
+$data = json_decode($raw, true);
 
+if (!is_array($data)) {
+    $data = $_POST;
+}
 
+$name = trim((string)($data['name'] ?? ''));
+$phone = trim((string)($data['phone'] ?? ''))
 
+echo json_encode([
+    'success' => true,
+    'got_name' => $name,
+    'got_phone' => $phone,
+], JSON_UNESCAPED_UNICODE);
