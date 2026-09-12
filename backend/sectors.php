6b911d0f229f5a92ce5263be8f51b223fb830402
Maryam-Musavi <maryam.a.musavi@gmail.com>
2026-09-12 10:34:35 +0330

    .

---
+++ backend/sectors.php
@@ -0,0 +1,14 @@
+<?php
+
+header('Content-Type: application/json; charset=UTF-8');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(['success' => false, 'message' => 'فقط GET مجاز است'],
+    JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$sector = ['واردات', 'صادرات', 'نمایندگی', 'سایر'];
+
+echo json_encode(['success' => true, 'sectors' => $sector], JSON_UNESCAPED_UNICODE);
